bc456e952017014612770d3c25184f5ffb14101e
Alejandro <alozano.m@hotmail.com>
2021-05-04 18:10:28 +0200

    M - 100. Creación de módulo de Auth
    Configuración inicial de TypeORM y conexión con la BD

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfig } from './config/typeorm.config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig), AuthModule],
   controllers: [],
   providers: [],
 })
